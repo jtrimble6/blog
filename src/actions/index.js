d198ca5bfb4a17269d3eb3194378bdbc9c7f0f41
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2021-06-30 10:42:23 -0500

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,37 @@
+import _ from 'lodash'
+import jsonPlaceholder from '../apis/jsonPlaceholder'
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts())
+    
+    // use lodash to find only unique IDs
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'))
+    // userIds.forEach(id => dispatch(fetchUser(id)))
+
+    //using lodash chain for above
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id=> dispatch(fetchUser(id)))
+        .value()
+}
+
+export const fetchPosts = () => 
+    async dispatch => {
+        const response = await jsonPlaceholder.get('/posts')
+        dispatch({ type: 'FETCH_POSTS', payload: response.data })
+    }
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`)
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+}
+
+
+// // make sure unique calls only made once using lodash memoize
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch)
+    
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`)
+//     dispatch({ type: 'FETCH_USER', payload: response.data })
+// })
